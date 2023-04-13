fac3263210765d5848aaf279179ca83b2ef615ee
Trukhova <antru231812@list.ru>
2023-04-13 23:49:02 +0300

    first review

--- tests/fixtures/fixture_user.py
+++ tests/fixtures/fixture_user.py
@@ -4,32 +4,33 @@ import pytest
 @pytest.fixture
 def user(django_user_model):
     return django_user_model.objects.create_user(
-        username='TestUser', password='1234567'
+        username="TestUser", password="1234567"
     )
 
 
 @pytest.fixture
 def user_2(django_user_model):
     return django_user_model.objects.create_user(
-        username='TestUser2', password='1234567'
+        username="TestUser2", password="1234567"
     )
 
 
 @pytest.fixture
 def another_user(django_user_model):
     return django_user_model.objects.create_user(
-        username='TestUserAnother', password='1234567'
+        username="TestUserAnother", password="1234567"
     )
 
 
 @pytest.fixture
 def token(user):
     from rest_framework_simplejwt.tokens import RefreshToken
+
     refresh = RefreshToken.for_user(user)
 
     return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
     }
 
 
